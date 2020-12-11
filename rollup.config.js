f524dd4e0494ad29e39ea5f6b18543e769226216
Boaz hoch <boaz@storm.black>
2020-12-11 18:28:12 +0200

    travis

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
