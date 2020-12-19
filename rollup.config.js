2956fce7d1f73a2dfd1b1c7a3a258de0ae8fdd7f
Boaz hoch <boaz@storm.black>
2020-12-19 18:44:09 +0200

    feat(): Fix formatter, change to pkg.json and suuport esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-// import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import ts from '@wessberg/rollup-plugin-ts'
 import pkg from './package.json'
@@ -9,8 +8,6 @@ import dev from 'rollup-plugin-dev'
 import koaBody from 'koa-body'
 import htmlTemplate from 'rollup-plugin-generate-html-template'
 import process from 'process'
-
-
 
 const isDev = process.env.BUILD === 'development'
 
@@ -22,7 +19,7 @@ const devPlugins = () => isDev ?
       template: 'index.html',
       target: 'index.html',
     }),
-    dev({dirs: ['dist'], port: 8081, extend(app, modules) { 
+    dev({dirs: ['dev/dist'], port: 8081, extend(app, modules) { 
       app.use(koaBody({ multipart: true }))
       app.use(modules.router.post('/log', (ctx, next) => {
         console.log(ctx.request.body)
@@ -35,17 +32,12 @@ const devPlugins = () => isDev ?
   const config =  [
     {
       input: 'src/index.ts',
-      // external: [
-      //   ...Object.keys(pkg.dependencies as unknown as any || {}),
-      //   ...Object.keys(pkg.peerDependencies as any || {}),
-      // ],
-      output: [
+      output: 
         {
           sourcemap: isDev,
-          file: pkg.main,
+          file: `${isDev ? 'dev/' : ''}${pkg.main}`,
           format: 'cjs',
         },
-      ],
       plugins: [
         ts(),
         resolve({ extensions }),
@@ -56,29 +48,10 @@ const devPlugins = () => isDev ?
     },
     {
       input: 'src/index.ts',
-      // external: [
-      //   ...Object.keys(pkg.dependencies as unknown as any || {}),
-      //   ...Object.keys(pkg.peerDependencies as any || {}),
-      // ],
       output: {
         sourcemap: isDev,
-          dir: 'dist',
-          format: 'esm',
-      },
-      plugins: [
-        ts(),
-        // resolve({ extensions }),
-        // commonjs(),
-        terser(),
-        ...devPlugins(),
-      ],
-    },
-    {
-      input: 'src/index.ts',
-      output: {
-        sourcemap: isDev,
-          file: pkg.module,
-          format: 'esm',
+        format: 'esm',
+        file: `${isDev ? 'dev/' : ''}${pkg.module}`,
       },
       plugins: [
         ts({ tsconfig: './tsconfig.es6.json' }),
--- src/TaggedLoggerFactory/TaggedLoggerFactory.ts
+++ src/TaggedLoggerFactory/TaggedLoggerFactory.ts
@@ -31,7 +31,9 @@ export default (params?: LoggerFactoryParams): ITaggedLogger  => {
 
   const logger = new Logger(logSender)
 
-  taggedLogger = new TaggedLogger(new TemplateString(params?.stringifier || new JsonStriginifer()), logger,   params?.logFormmaterOptions && new LogFormatter(logFormatterOptionsFcatory(params.logFormmaterOptions)))
+  const logFormatters = params?.logFormmaterOptions ? logFormatterOptionsFcatory(params?.logFormmaterOptions) : undefined
+
+  taggedLogger = new TaggedLogger(new TemplateString(params?.stringifier || new JsonStriginifer()), logger, new LogFormatter(logFormatters))
 
   return taggedLogger
 }
